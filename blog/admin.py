2e1090a762df7c25ac10b1fb97284f3ad065b253
vallka <vallka@vallka.com.com>
2022-01-01 21:21:11 +0000

    search

--- blog/admin.py
+++ blog/admin.py
@@ -13,4 +13,25 @@ admin.site.register(Category)
 @admin.register(Post)
 class PostAdmin(MarkdownxModelAdmin):
     list_display = ['id','slug','title','blog','email','created_dt']
-    search_fields = ['title','text', ]
+    search_fields = ['title', ]
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
+        
+        #print (queryset.query)
+
+        sql = "SELECT id from `blog_post`"
+        sql += " where match(title,text) against (%s in boolean mode) limit 0,100"
+
+        queryset2 = self.model.objects.raw(sql,[search_term])
+
+        print( len(queryset2))
+        if len(queryset2)>0:
+            s=[]
+            for row in queryset2:
+                s.append(row.id)
+
+            queryset3 = self.model.objects.filter(id__in=s)
+            return queryset | queryset3, may_have_duplicates
+
+        return queryset, may_have_duplicates    
--- gellifinsta/admin.py
+++ gellifinsta/admin.py
@@ -13,9 +13,30 @@ class ProductsAdmin(admin.ModelAdmin):
 @admin.register(Gellifinsta)
 class GellifinstaAdmin(admin.ModelAdmin):
     list_display = ['shortcode','taken_at_datetime','is_video','image_tag',]
-    search_fields = ['caption','tags', 'shortcode','id']
+    search_fields = ['shortcode','id']
     #list_filter = ['tags',]
     fields = ['shortcode','taken_at_datetime','created_dt','updated_dt','username','is_active','is_video',
                 'file_path','caption','tags','url','image_tag']
 
     readonly_fields = ['image_tag','created_dt','updated_dt']
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
+        
+        #print (queryset.query)
+
+        sql = "SELECT id from `gellifinsta_gellifinsta`"
+        sql += " where match(tags) against (%s in boolean mode) limit 0,100"
+
+        queryset2 = self.model.objects.raw(sql,[search_term])
+
+        print( len(queryset2))
+        if len(queryset2)>0:
+            s=[]
+            for row in queryset2:
+                s.append(row.id)
+
+            queryset3 = self.model.objects.filter(id__in=s)
+            return queryset | queryset3, may_have_duplicates
+
+        return queryset, may_have_duplicates    
