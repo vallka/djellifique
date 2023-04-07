19228cf49d62958921c6c766b75fb0fe9bf057b9
vallka <vallka@vallka.com.com>
2023-04-07 13:56:30 +0100

    urls

--- blog/urls.py
+++ blog/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('search/', SearchView.as_view(), name='search'),
     path('newsletter/<str:slug>/<str:lang>/', NewsletterView.as_view(), name='newsletter'),
     path('newsletter/<str:slug>/', NewsletterView.as_view(), name='newsletter_en'),
-    path('<str:slug>/', PostView.as_view(), name='post_en'),
-    path('<str:slug>/<str:lang>/', PostView.as_view(), name='post'),
     #path('category/<str:slug>/', ListView.as_view(), name='post-by-cat'),
     path('category/<str:slug>/', HomeView.as_view(), name='post-by-cat'),
     path('translate/<str:slug>', translate, name='translate'),
+    path('<str:slug>/', PostView.as_view(), name='post_en'),
+    path('<str:slug>/<str:lang>/', PostView.as_view(), name='post'),
 ]
